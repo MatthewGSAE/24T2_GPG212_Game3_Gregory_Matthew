bbd03aa8202b3cabdff4e1b727eb26e588ac3b2c
MatthewGSAE <136556271+MatthewGSAE@users.noreply.github.com>
2024-02-23 16:59:18 +1100

    Initial commit

---
+++ Assets/CarMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    [Header("Wheels collider")]
+    public WheelCollider frontRightWheelCollider;
+    public WheelCollider frontLeftWheelCollider;
+    public WheelCollider backRightWheelCollider;
+    public WheelCollider backLeftWheelCollider;
+
+    [Header("Wheels Transform")]
+    public Transform frontRightWheelTransform;
+    public Transform frontLeftWheelTransform;
+    public Transform backRightWheelTransform;
+    public Transform backLeftWheelTransform;
+
+    [Header("Car Engine")]
+    public float accelarationForce = 300f;
+    public float breakingForce = 3000f;
+    private float presentBreakForce = 0f;
+    private float presentAcceleration = 0f;
+
+    private void Update()
+    {
+        MoveCar();
+    }
+
+    private void MoveCar()
+    {
+        frontLeftWheelCollider.motorTorque = presentAcceleration;
+        frontRightWheelCollider.motorTorque = presentAcceleration;
+        backLeftWheelCollider.motorTorque = presentAcceleration;
+        backRightWheelCollider.motorTorque = presentAcceleration;
+
+        presentAcceleration = accelarationForce * Input.GetAxis("Vertical");
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# 24T2_GPG212_Game3_Gregory_Matthew
+ 
