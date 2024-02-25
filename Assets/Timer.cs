bb4ba2caebb1c4a1612b6e72303ad5960e67814d
MatthewGSAE <136556271+MatthewGSAE@users.noreply.github.com>
2024-02-25 22:51:38 +1100

    starting garage

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -10,6 +10,12 @@ public class Timer : MonoBehaviour
 
     [Header("things to stop")]
     public CarMovement carMovement;
+    public CarMovement carMovement1;
+    public CarMovement carMovement2;
+    public CarMovement carMovement3;
+    public CarMovement carMovement4;
+    public CarMovement carMovement5;
+    public CarMovement carMovement6;
     public OpponentCar opponentCar;
     public OpponentCar opponentCar1;
     public OpponentCar opponentCar2;
@@ -30,6 +36,12 @@ public class Timer : MonoBehaviour
         if(countDownTimer > 1)
         {
             carMovement.accelarationForce = 0f;
+            carMovement1.accelarationForce = 0f;
+            carMovement2.accelarationForce = 0f;
+            carMovement3.accelarationForce = 0f;
+            carMovement4.accelarationForce = 0f;
+            carMovement5.accelarationForce = 0f;
+            carMovement6.accelarationForce = 0f;
             opponentCar.movingSpeed = 0f;
             opponentCar1.movingSpeed = 0f;
             opponentCar2.movingSpeed = 0f;
@@ -39,6 +51,12 @@ public class Timer : MonoBehaviour
         else if(countDownTimer == 0)
         {
             carMovement.accelarationForce = 300f;
+            carMovement1.accelarationForce = 300f;
+            carMovement2.accelarationForce = 300f;
+            carMovement3.accelarationForce = 300f;
+            carMovement4.accelarationForce = 300f;
+            carMovement5.accelarationForce = 300f;
+            carMovement6.accelarationForce = 300f;
             opponentCar.movingSpeed = 14f;
             opponentCar1.movingSpeed = 15f;
             opponentCar2.movingSpeed = 16f;
